eb794628ba77892663016902031e0df7d5edabae
sschoon <stefan.schoon@hotmail.com>
2024-12-20 16:32:48 +0100

    Update start_emulator.cmd

--- start_emulator.cmd
+++ start_emulator.cmd
@@ -1,1 +1,2 @@
-START "" /B "%ANDROID_HOME%\emulator\emulator.exe" -avd Pixel_3a_XL_API_28 -netdelay none -netspeed full
+set ANDROID_EMULATOR_WAIT_TIME_BEFORE_KILL=10
+"%ANDROID_HOME%\emulator\emulator.exe" -avd Pixel_3a_XL_API_28 -netdelay none -netspeed full
